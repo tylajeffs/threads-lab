d5704261b8ed07d3ee57c6c4a9b2ad3db123432b
Tyla Jeffs <tylajeffs@gmail.com>
2022-10-24 22:36:37 +0000

    initial commit

---
+++ threads.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
+#include <fcntl.h>
+#include <dirent.h>
+#include <errno.h>
+
+
+
+
+//main function
+int main(int argc, char **argv) {
+
+    //start
+    printf("Launching 4 threads... \n");
+    
+
+
+
+}
